cb854711d8c36e2c650fedca84a69620a17daa2c
ZyC0R3 <admin@zycore.co.uk>
2021-05-13 23:58:08 +0100

    3.1.0

--- index.js
+++ index.js
@@ -1,141 +1,169 @@
-var querystring = require('querystring');
+const querystring = require("querystring");
 
-var got = require('got');
+const got = require("got");
 
-var languages = require('./languages');
+const languages = require("./languages");
 
-function extract(key, res) {
-    var re = new RegExp(`"${key}":".*?"`);
-    var result = re.exec(res.body);
-    if (result !== null) {
-        return result[0].replace(`"${key}":"`, '').slice(0, -1);
-    }
-    return '';
-}
+function translate (text, opts, gotopts)
+{
 
-function translate(text, opts, gotopts) {
-    opts = opts || {};
-    gotopts = gotopts || {};
-    var e;
-    [opts.from, opts.to].forEach(function (lang) {
-        if (lang && !languages.isSupported(lang)) {
-            e = new Error();
-            e.code = 400;
-            e.message = 'The language \'' + lang + '\' is not supported';
-        }
-    });
-    if (e) {
-        return new Promise(function (resolve, reject) {
-            reject(e);
-        });
-    }
+   opts = opts || {};
+   gotopts = gotopts || {};
+   let e;
+   [opts.from, opts.to].forEach(function (lang)
+   {
 
-    opts.from = opts.from || 'auto';
-    opts.to = opts.to || 'en';
-    opts.tld = opts.tld || 'com';
+      if (lang && !languages.isSupported(lang))
+      {
 
-    opts.from = languages.getCode(opts.from);
-    opts.to = languages.getCode(opts.to);
+         e = new Error();
+         e.code = 400;
+         e.message = "The language '" + lang + "' is not supported";
 
-    var url = 'https://translate.google.' + opts.tld;
-    return got(url, gotopts).then(function (res) {
-        var data = {
-            'rpcids': 'MkEWBc',
-            'f.sid': extract('FdrFJe', res),
-            'bl': extract('cfb2h', res),
-            'hl': 'en-US',
-            'soc-app': 1,
-            'soc-platform': 1,
-            'soc-device': 1,
-            '_reqid': Math.floor(1000 + (Math.random() * 9000)),
-            'rt': 'c'
-        };
+      }
 
-        return data;
-    }).then(function (data) {
-        url = url + '/_/TranslateWebserverUi/data/batchexecute?' + querystring.stringify(data);
-        gotopts.body = 'f.req=' + encodeURIComponent(JSON.stringify([[['MkEWBc', JSON.stringify([[text, opts.from, opts.to, true], [null]]), null, 'generic']]])) + '&';
-        gotopts.headers['content-type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+   });
+   if (e)
+   {
 
-        return got.post(url, gotopts).then(function (res) {
-            var json = res.body.slice(6);
-            var length = '';
+      return new Promise(function result (resolve, reject)
+      {
 
-            var result = {
-                text: '',
-                pronunciation: '',
-                from: {
-                    language: {
-                        didYouMean: false,
-                        iso: ''
-                    },
-                    text: {
-                        autoCorrected: false,
-                        value: '',
-                        didYouMean: false
-                    }
-                },
-                raw: ''
-            };
+         reject(e);
 
-            try {
-                length = /^\d+/.exec(json)[0];
-                json = JSON.parse(json.slice(length.length, parseInt(length, 10) + length.length));
-                json = JSON.parse(json[0][2]);
-                result.raw = json;
-            } catch (e) {
-                return result;
-            }
+      });
 
-            if (json[1][0][0][5] === undefined || json[1][0][0][5] === null) {
-                // translation not found, could be a hyperlink or gender-specific translation?
-                result.text = json[1][0][0][0];
-            } else {
-                json[1][0][0][5].forEach(function (obj) {
-                    if (obj[0]) {
-                        result.text += obj[0];
-                    }
-                });
-            }
-            result.pronunciation = json[1][0][0][1];
+   }
 
-            // From language
-            if (json[0] && json[0][1] && json[0][1][1]) {
-                result.from.language.didYouMean = true;
-                result.from.language.iso = json[0][1][1][0];
-            } else if (json[1][3] === 'auto') {
-                result.from.language.iso = json[2];
-            } else {
-                result.from.language.iso = json[1][3];
-            }
+   opts.from = opts.from || "auto";
+   opts.to = opts.to || "en";
+   opts.tld = opts.tld || "com";
 
-            // Did you mean & autocorrect
-            if (json[0] && json[0][1] && json[0][1][0]) {
-                var str = json[0][1][0][0][1];
+   opts.from = languages.getCode(opts.from);
+   opts.to = languages.getCode(opts.to);
 
-                str = str.replace(/<b>(<i>)?/g, '[');
-                str = str.replace(/(<\/i>)?<\/b>/g, ']');
+   let url = "https://translate.google." + opts.tld + "/translate_a/t";
+   const data = {
+      "client": opts.client || "dict-chrome-ex",
+      "sl": opts.from,
+      "tl": opts.to,
+      "hl": opts.to,
+      "dt": ["at", "bd", "ex", "ld", "md", "qca", "rw", "rm", "ss", "t"],
+      "ie": "UTF-8",
+      "oe": "UTF-8",
+      "otf": 1,
+      "ssel": 0,
+      "tsel": 0,
+      "kc": 7,
+      "q": text
+   };
 
-                result.from.text.value = str;
+   url = url + "?" + querystring.stringify(data);
 
-                if (json[0][1][0][2] === 1) {
-                    result.from.text.autoCorrected = true;
-                } else {
-                    result.from.text.didYouMean = true;
-                }
-            }
+   return got(url, gotopts).then(function (res)
+   {
 
-            return result;
-        }).catch(function (err) {
-            err.message += `\nUrl: ${url}`;
-            if (err.statusCode !== undefined && err.statusCode !== 200) {
-                err.code = 'BAD_REQUEST';
-            } else {
-                err.code = 'BAD_NETWORK';
-            }
-            throw err;
-        });
-    });
+      const result =
+         {
+            "text": "",
+            "pronunciation": "",
+            "from":
+            {
+               "language":
+               {
+                  "didYouMean": false,
+                  "iso": ""
+               },
+               "text":
+               {
+                  "autoCorrected": false,
+                  "value": "",
+                  "didYouMean": false
+               }
+            },
+            "raw": ""
+         };
+
+      if (opts.raw)
+      {
+
+         result.raw = res.body;
+
+      }
+
+      const body = JSON.parse(res.body);
+
+      body.sentences.forEach(function (obj)
+      {
+
+         if (obj.trans)
+         {
+
+            result.text += obj.trans;
+
+         }
+         if (obj.translit)
+         {
+
+            result.pronunciation += obj.translit;
+
+         }
+
+      });
+
+      if (body.src === body.ld_result.srclangs[0])
+      {
+
+         result.from.language.iso = body.ld_result.srclangs[0];
+
+      }
+      else
+      {
+
+         result.from.language.didYouMean = true;
+         result.from.language.iso = body.ld_result.srclangs[0];
+
+      }
+
+      if (body.spell)
+      {
+
+         let str = body.spell.spell_html_res;
+
+         str = str.replace(/<b><i>/g, "[");
+         str = str.replace(/<\/i><\/b>/g, "]");
+
+         result.from.text.value = str;
+
+            // result.from.text.autoCorrected is always false using '/translate_a/t'
+         result.from.text.didYouMean = true;
+
+      }
+
+      return result;
+
+   }).
+   catch(function error (err)
+
+   {
+
+      err.message += `\nUrl: ${url}`;
+      if (err.statusCode !== undefined && err.statusCode !== 200)
+      {
+
+         err.code = "BAD_REQUEST";
+
+      }
+      else
+      {
+
+         err.code = "BAD_NETWORK";
+
+      }
+      throw err;
+
+   });
+
 }
 
 module.exports = translate;
